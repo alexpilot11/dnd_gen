cf7e9e71653ff05ecc4fb18b38e94e437be6584f
Alex Channell <alexpilot11@gmail.com>
2020-12-22 17:05:40 -0500

    class-based planet, no new functionality

---
+++ gen_planet/enums.py
@@ -0,0 +1,78 @@
+from enum import Enum
+
+
+class BaseEnum(Enum):
+
+    @classmethod
+    def values(cls):
+        return [v.value for v in cls]
+
+
+class Plane(BaseEnum):
+    MATERIAL = 'Material'
+    FEYWILD = 'Feywild'
+    SHADOWFELL = 'Shadowfell'
+    MECHANUS = 'Mechanus'
+    LIMBO = 'Limbo'
+    PANDEMONIUM = 'Pandemonium'
+    THE_ABYSS = 'The Abyss'
+    CARCERI = 'Carceri'
+    HADES = 'Hades'
+    GEHENNA = 'Gehenna'
+    THE_NINE_HELLS = 'The Nine Hells'
+    ACHERON = 'Acheron'
+    ARCADIA = 'Arcadia'
+    MOUNT_CELESTIA = 'Mount Celestia'
+    BYTOPIA = 'Bytopia'
+    ELYSIUM = 'Elysium'
+    THE_BEASTLANDS = 'The Beastlands'
+    ARBOREA = 'Arborea'
+    YSGARD = 'Ysgard'
+
+
+class Size(BaseEnum):
+    SMALL = 'Small'
+    MEDIUM = 'Medium'
+    LARGE = 'Large'
+    HUGE = 'Huge'
+    GIANT = 'Giant'
+
+
+class Population(BaseEnum):
+    SMALL = 'Small'
+    MEDIUM = 'Medium'
+    LARGE = 'Large'
+    HUGE = 'Huge'
+    GIANT = 'Giant'
+
+
+class Conflict(BaseEnum):
+    WAR = 'War'
+    POLITICAL_LAND_BOUNDARIES = 'Political land boundaries'
+    CITIZEN_LAND_BOUNDARIES = 'Citizen land boundaries'
+    THEFT = 'Theft'
+    CRIME_GANGS = 'Crime - gangs'
+    CRIME_SLAVERY = 'Crime - slavery'
+    CRIME_DOMESTIC_ABUSE = 'Crime - domestic abuse'
+    POLITIAL_POWER_STRUGGLE = 'Politial power struggle'
+    NONE = None
+
+
+class RawMaterials(BaseEnum):
+    PRECIOUS_METAL_ORE = 'Precious metal ore'
+    PRECIOUS_GEMS = 'Precious gems'
+    USEFUL_METAL_ORE = 'Useful metal ore'
+    VEGETATION = 'Vegetation'
+    WOOD = 'Wood'
+    MAGIC = 'Magic metal'
+    GAS_MEDICAL = 'Gas - medical'
+    FUEL_GAS = 'Fuel - gas'
+    FUEL_COAL_LIKE = 'Fuel - coal-like'
+    NONE = None
+
+
+class Economy(BaseEnum):
+    RAW_MATERIALS = 'Raw materials'
+    MERCHANT = 'Merchant'
+    BLACK_MARKET = 'Black market'
+    NONE = None
---
+++ gen_planet/planet.py
@@ -0,0 +1,64 @@
+import random
+import requests
+
+from majormode.utils.namegen import NameGeneratorFactory
+
+from gen_planet import enums
+
+
+class Planet:
+
+    _generate = NameGeneratorFactory.get_instance(
+        NameGeneratorFactory.Language.Greek
+    ).generate_name
+
+    def __init__(self):
+      self.gen_plane()
+      self.gen_name()
+      self.gen_size()
+      self.gen_plane()
+      self.gen_population()
+      self.gen_conflict()
+      self.gen_raw_materials()
+      self.gen_economy()
+
+    def display(self):
+        print('-----------------------------------------')
+        print(f'{"Plane:":<20}{self.plane.value}')
+        print(f'{"Name:":<20}{self.name}')
+        print(f'{"Size:":<20}{self.size.value}')
+        print(f'{"Plane:":<20}{self.plane.value}')
+        print(f'{"Population:":<20}{self.population.value}')
+        print(f'{"Conflict:":<20}{self.conflict.value}')
+        print(f'{"Raw Materials:":<20}{self.raw_materials.value}')
+        print(f'{"Economy:":<20}{self.economy.value}')
+        print('-----------------------------------------')
+
+    def gen_name(self):
+        word_count = random.choice([1]*3 + [2])  # Shorter names have higher weights
+        self.name = ' '.join([self._generate() for _ in range(word_count)])
+
+    def gen_size(self):
+        # TODO: make different percentages for planet sizes
+        self.size = random.choice(list(enums.Size))
+
+    def gen_plane(self):
+        self.plane = random.choice(list(enums.Plane))
+
+
+    def gen_population(self):
+        # TODO: conditionals
+        self.population = random.choice(list(enums.Population))
+
+
+    def gen_conflict(self):
+        self.conflict = random.choice(list(enums.Conflict) + [enums.Conflict.NONE] * 5)
+
+
+    def gen_raw_materials(self):
+        self.raw_materials = random.choice(
+            list(enums.RawMaterials) + [enums.RawMaterials.NONE] * 2
+        )
+
+    def gen_economy(self):
+        self.economy = random.choice(list(enums.Economy) + [enums.RawMaterials.NONE])
