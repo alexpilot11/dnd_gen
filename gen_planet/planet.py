03ca6b36daa56982feafc0a1c0a1956210e80573
Alex Channell <alexpilot11@gmail.com>
2020-12-22 18:23:15 -0500

    first pass on conditional attrs

--- gen_planet/planet.py
+++ gen_planet/planet.py
@@ -16,27 +16,23 @@ class Planet:
 
 
     def __init__(self):
-        self.gen_plane()
         self.gen_name()
-        self.gen_size()
         self.gen_plane()
-        self.gen_population()
-        self.gen_conflict()
+        self.gen_size()
         self.gen_raw_materials()
-        self.gen_economy()
+        self.gen_population()
 
     def print_attr(self, attr, value):
         print(f'{f"{attr}:":{self.PADDING}}{value}')
 
     def display(self):
         print(self.OUTLINE)
-        self.print_attr('Plane', self.plane.value)
         self.print_attr('Name', self.name)
-        self.print_attr('Size', self.size.value)
         self.print_attr('Plane', self.plane.value)
+        self.print_attr('Size', self.size.value)
+        self.print_attr('Raw Materials', self.raw_materials.value)
         self.print_attr('Population', self.population.value)
         self.print_attr('Conflict', self.conflict.value)
-        self.print_attr('Raw Materials', self.raw_materials.value)
         self.print_attr('Economy', self.economy.value)
         print(self.OUTLINE)
 
@@ -46,22 +42,32 @@ class Planet:
         if random.randint(0, 10) > 8:
             self.name += f' {random.choice(["I", "II", "III", "IV", "V"])}'
 
-    def gen_size(self):
-        self.size = random.choice(list(enums.Size))
-
     def gen_plane(self):
         self.plane = random.choice(list(enums.Plane))
 
-    def gen_population(self):
-        self.population = random.choice(list(enums.Population))
-
-    def gen_conflict(self):
-        self.conflict = random.choice(list(enums.Conflict) + [enums.Conflict.NONE] * 5)
+    def gen_size(self):
+        self.size = random.choice(list(enums.Size))
 
     def gen_raw_materials(self):
         self.raw_materials = random.choice(
             list(enums.RawMaterials) + [enums.RawMaterials.NONE] * 2
         )
 
+    def gen_population(self):
+        choices = list(enums.Population)
+        if self.size != enums.Size.GIANT:
+            choices.remove(enums.Population.GIANT)
+
+        self.population = random.choice(choices)
+        if self.population.value is not None:
+            self.gen_conflict()
+            self.gen_economy()
+
+    def gen_conflict(self):
+        self.conflict = random.choice(list(enums.Conflict) + [enums.Conflict.NONE] * 5)
+
     def gen_economy(self):
-        self.economy = random.choice(list(enums.Economy) + [enums.RawMaterials.NONE])
+        choices = list(enums.Economy) + [enums.Economy.NONE]
+        if self.raw_materials is None:
+            choices.remove(enums.Economy.RAW_MATERIALS)
+        self.economy = random.choice(choices)
